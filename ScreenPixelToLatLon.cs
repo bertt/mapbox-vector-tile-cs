6397c3a5091ef14ad63b3e71a2d89960262f8a75
bertt <btemme@hotmail.com>
2015-07-07 09:51:05 +0200

    convert to latlong

--- ScreenPixelToLatLon.cs
+++ ScreenPixelToLatLon.cs
@@ -1,32 +1,18 @@
-﻿namespace Mapbox.Vectors
+﻿using System;
+
+namespace Mapbox.Vectors
 {
     public class ScreenPixelToLatLon
     {
-        public static Coordinate Convert(long x, long y, int zoomLevel)
+        public static Coordinate Convert(int extent, long xtile, long ytile, int z)
         {
-            /**
-            var g = (y - this.zc[zoom]) / (-this.Cc[zoom]);
-            var lon = (px[0] - this.zc[zoom]) / this.Bc[zoom];
-            var lat = R2D * (2 * Math.atan(Math.exp(g)) - 0.5 * Math.PI);
-            return [lon, lat];
-             */
+            var size = extent*Math.Pow(2, z);
+            var x0 = extent*xtile;
+            var y0 = extent*ytile;
+
+            // todo: add more code from 
             return null;
         }
     }
 }
 
-
-/**
- * https://github.com/mapbox/mapbox-studio/blob/5ac2ead1e523b24c8b8ad8655babb66389166e87/ext/sphericalmercator.js
- * 
- * 
- * // Convert screen pixel value to lon lat
-//
-// - `px` {Array} `[x, y]` array of geographic coordinates.
-// - `zoom` {Number} zoom level.
-SphericalMercator.prototype.ll = function(px, zoom) {
-    var g = (px[1] - this.zc[zoom]) / (-this.Cc[zoom]);
-    var lon = (px[0] - this.zc[zoom]) / this.Bc[zoom];
-    var lat = R2D * (2 * Math.atan(Math.exp(g)) - 0.5 * Math.PI);
-    return [lon, lat];
-};*/
