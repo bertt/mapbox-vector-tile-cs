b1071ab83b1751d75331eff0ed6cf7d021eff202
bert <btemme@hotmail.com>
2016-10-17 22:01:16 +0200

    add extension methods

---
+++ src/ExtensionMethods/VectorTileFeatureExtensions.cs
@@ -0,0 +1,14 @@
+﻿using GeoJSON.Net.Feature;
+
+namespace mapbox.vector.tile
+{
+	public static class VectorTileFeatureExtensions
+	{
+		public static Feature ToGeoJSON(this VectorTileFeature vectortileFeature)
+		{
+			// todo: create convertor
+			return null;
+		}
+	}
+}
+
---
+++ src/ExtensionMethods/VectorTileLayerExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace mapbox.vector.tile
+{
+	public static class VectorTileLayerExtensions
+	{
+		public static GeoJSON.Net.Feature.FeatureCollection ToGeoJSON(this VectorTileLayer vectortileLayer)
+		{
+			// todo: create convertor
+			return null;
+		}
+	}
+}
+
