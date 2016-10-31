df16defc5f8fcac890bbff19c50c44595c491570
bert <btemme@hotmail.com>
2016-10-31 12:04:01 +0100

    add MultiPolygon and MultiLine unit test, test coverage 86%

--- tests/mapbox.vector.tile.tests/MultiGeomtryTests.cs
+++ tests/mapbox.vector.tile.tests/MultiGeomtryTests.cs
@@ -18,6 +18,123 @@ namespace mapbox.vector.tile.tests
         }
 
         [Test]
+        public void TestToGeoJsonMultiPolygonFeature()
+        {
+            // arrange
+            const string mapboxfile = "mapbox.vector.tile.tests.testdata.multi-polygon.pbf";
+            var feature = GeoJSONFromFixture(mapboxfile);
+
+            // act
+            var json = JsonConvert.SerializeObject(feature);
+            var actualResult = JObject.Parse(json);
+            // todo: why is there a difference of 0.03 wrt the mapbox js test?
+            var expectedResult = JObject.Parse(@"
+                {
+                  'geometry': {
+                    'coordinates': [
+                      [
+                        [
+                          [
+                            0.966796875,
+                            0.0
+                          ],
+                          [
+                            0.0,
+                            0.0
+                          ],
+                          [
+                            0.966796875,
+                            0.96675099976664569
+                          ],
+                          [
+                            0.966796875,
+                            0.0
+                          ]
+                        ]
+                      ],
+                      [
+                        [
+                          [
+                            0.966796875,
+                            0.0
+                          ],
+                          [
+                            0.0,
+                            0.0
+                          ],
+                          [
+                            0.966796875,
+                            0.96675099976664569
+                          ],
+                          [
+                            0.966796875,
+                            0.0
+                          ]
+                        ]
+                      ]
+                    ],
+                    'type': 'MultiPolygon'
+                  },
+                  'id': '1',
+                  'properties': {},
+                  'type': 'Feature'
+                }
+            ");
+
+            // assert
+            Assert.IsTrue(JToken.DeepEquals(actualResult, expectedResult));
+        }
+
+
+
+        [Test]
+        public void TestToGeoJsonMultiLineFeature()
+        {
+            // arrange
+            const string mapboxfile = "mapbox.vector.tile.tests.testdata.multi-line.pbf";
+            var feature = GeoJSONFromFixture(mapboxfile);
+
+            // act
+            var json = JsonConvert.SerializeObject(feature);
+            var actualResult = JObject.Parse(json);
+            // todo: why is there a difference of 0.03 wrt the mapbox js test?
+            var expectedResult = JObject.Parse(@"
+                {
+                  'geometry': {
+                    'coordinates': [
+                      [
+                        [
+                          0.966796875,
+                          2.0210651187669839
+                        ],
+                        [
+                          2.98828125,
+                          4.0396178267684348
+                        ]
+                      ],
+                      [
+                        [
+                          5.009765625,
+                          5.9657536710655421
+                        ],
+                        [
+                          7.03125,
+                          7.97219771438688
+                        ]
+                      ]
+                    ],
+                    'type': 'MultiLineString'
+                  },
+                  'id': '1',
+                  'properties': {},
+                  'type': 'Feature'
+                }
+            ");
+            // assert
+            Assert.IsTrue(JToken.DeepEquals(actualResult, expectedResult));
+        }
+
+        [Test]
         public void TestToGeoJsonMultiPointFeature()
         {
             const string mapboxfile = "mapbox.vector.tile.tests.testdata.multi-point.pbf";
@@ -48,6 +165,5 @@ namespace mapbox.vector.tile.tests
             ");
             Assert.IsTrue(JToken.DeepEquals(actualResult, expectedResult));
         }
-
     }
 }
