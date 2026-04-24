59f4c7feb4ba0f2548c2cbed3462f92c26505b99
Bert Temme <btemme@hotmail.com>
2026-04-24 10:04:56 +0200

    simplify benchmark test

--- benchmark/Program.cs
+++ benchmark/Program.cs
@@ -7,18 +7,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Type benchmarkType;
-        if (args.Length == 0)
-        {
-            benchmarkType = typeof(ParsingBenchmark);
-        }
-        else
-        {
-            var fullName = "mapbox.vector.tile.benchmark." + args[0];
-            benchmarkType = Type.GetType(fullName) ?? Type.GetType(fullName + "Benchmark") ?? throw new ArgumentException($"Unrecognised type '{args[0]}'");
-        }
-
-        var summary = BenchmarkRunner.Run(benchmarkType);
+        BenchmarkRunner.Run<ParsingBenchmark>();
+        // BenchmarkRunner.Run<BooleanAttributesBenchmark>();
         Console.ReadKey();
     }
 }
