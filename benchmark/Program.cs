8969e152ebea8f9d321edd1836be0df6ed8a1a9b
Bert Temme <btemme@hotmail.com>
2026-04-24 10:05:14 +0200

    enable all benchmarks

--- benchmark/Program.cs
+++ benchmark/Program.cs
@@ -8,7 +8,7 @@ class Program
     static void Main(string[] args)
     {
         BenchmarkRunner.Run<ParsingBenchmark>();
-        // BenchmarkRunner.Run<BooleanAttributesBenchmark>();
+        BenchmarkRunner.Run<BooleanAttributesBenchmark>();
         Console.ReadKey();
     }
 }
